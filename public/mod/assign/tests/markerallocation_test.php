b6cf8bf7d4fa6cc94c97013fed888258eee18a68
synbulatov <fidandestro@yandex.ru>
2025-12-15 13:18:40 +0300

    MDL-87464: delete course_add_cm_to_section() function because course module already created

--- public/mod/assign/tests/markerallocation_test.php
+++ public/mod/assign/tests/markerallocation_test.php
@@ -72,8 +72,6 @@ final class markerallocation_test extends \advanced_testcase {
 
         $assignelement = $this->getDataGenerator()->create_module('assign', $record, $modulesettings);
 
-        $coursesectionid = course_add_cm_to_section($this->course->id, $assignelement->id, 1, null, 'assign');
-
         // Adding users to the course.
         $userdata = array();
         $userdata['firstname'] = 'teacher1';
